664b14faacfde74845537205f4af55d4d46ff719
Luis Colorado <luiscoloradourcola@gmail.com>
2025-10-14 06:08:03 -0500

    Documentacion para plugin0.c, que ahora llama a _init()
    Signed-off-by: Luis Colorado <luiscoloradourcola@gmail.com>

--- plugin0.c
+++ plugin0.c
@@ -1,6 +1,18 @@
+/* plugin0.c -- Sample test plugin to run in hoc.
+ * Author: Edward Rivas <rivastkw@gmail.com>
+ * Date: Tue Oct 14 06:01:58 -05 2025
+ * Copyright: (c) 2025 Edward Rivas.  All rights reserved.
+ * License: BSD
+ */
 
 #include <stdio.h>
 
+/* La rutina que dlopen() ejecuta automaticamente se llama
+ * _init, pero es necesario enlazar el .so llamando al
+ * linker ld(1) directamente, para que no cargue el modulo
+ * crti0.o (que contiene un _init()).
+ * Esto no afecta al modulo, que incluso sera mas peque;o,
+ * de hecho. Esto se hace asi en el Makefile ahora. */
 int _init()
 {
 	fprintf(stderr, "Test plugin v1.0\n");
