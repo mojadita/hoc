5cef5b62c0d5d7c683b14d79535875b4e19087d0
Luis Colorado <luiscoloradourcola@gmail.com>
2025-10-15 13:31:31 +0300

    plugins.h was required to compile plugin0 but not in git
    Signed-off-by: Luis Colorado <luiscoloradourcola@gmail.com>

---
+++ plugins.h
@@ -0,0 +1,18 @@
+/* plugins.h -- definiciones y constantes para el desarrollo
+ * de plugins.
+ * Author: Luis Colorado <luiscoloradourcola@gmail.com>
+ * Date: Tue Oct 14 14:47:50 -05 2025
+ * copyright: (c) 2025 Luis Colorado.  All rights reserved.
+ * License: BSD
+ */
+#ifndef PLUGINS_H_bf736efa_a936_11f0_9bf4_0023ae68f329
+#define PLUGINS_H_bf736efa_a936_11f0_9bf4_0023ae68f329
+
+#include "builtins.h"
+#include "symbol.h"
+#include "instr.h"
+#include "types.h"
+#include "code.h"
+#include "cellP.h"
+
+#endif /* PLUGINS_H_bf736efa_a936_11f0_9bf4_0023ae68f329 */
